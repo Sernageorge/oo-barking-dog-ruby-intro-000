35ca26eb025f2a4360c393fe3c8eca71b9e574ef
George Serna <sernageorge@gmail.com>
2021-02-25 02:37:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,23 @@
 # Your code goes here!
 class Dog
+  def initialize(name, bark = "Woof!")
+    @name = name
+    @bark = bark
+  end
 
+  def name=(name)
+    @name=name
+  end
+
+  def name
+    @name
+  end
+
+  def bark=(bark = "Woof!")
+    @bark=bark
+  end
+
+  def bark
+    @bark
+  end
 end
