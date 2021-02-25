327a335d04bcbb1e9c37210101dc6617dad93247
George Serna <sernageorge@gmail.com>
2021-02-25 02:29:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog.
+  def name=(name)
+    @name=name
+  end
+
+  def name
+    @name
+  end
+
+  def bark=(bark = "Woof!")
+    @bark=bark
+  end
+
+  def bark
+    @bark
+  end
+end
